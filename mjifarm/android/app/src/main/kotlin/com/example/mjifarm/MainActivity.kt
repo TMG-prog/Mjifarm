4383970813568449844b628b5941ba900a3a5911
TMG-prog <tracymuguregitau@gmail.com>
2025-05-24 13:59:07 +0300

    m

---
+++ mjifarm/android/app/src/main/kotlin/com/example/mjifarm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mjifarm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mjifarm/lib/splash.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:mjifarm/Login.dart';
+import 'package:mjifarm/landing.dart';
+import 'package:mjifarm/main.dart';
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    _controller.forward();
+
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 10)); // Increased duration
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) =>  LandingPage()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFE8F6EE), Color(0xFFFFFFFF)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/Mjifarms.png',
+                  width: 500,
+                  height: 500,
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
