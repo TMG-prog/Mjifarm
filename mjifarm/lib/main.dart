552883a8f6c02090eddcc04ed74e478e31ae171e
Jimmy-szn <jimmykariukikimotho2@gmail.com>
2025-06-20 13:47:52 +0300

    Fixed the main page to accomodate changes

--- mjifarm/lib/main.dart
+++ mjifarm/lib/main.dart
@@ -1,15 +1,15 @@
-// main.dart
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mjifarm/mainscreen.dart';
-import 'package:mjifarm/newplant.dart';
-import 'package:mjifarm/plants.dart';
+import 'splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,16 +20,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MjiFarms',
       theme: ThemeData(
+        fontFamily: 'NotoSerif',
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const Splash(),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => MainScreen(),
-        '/myplants': (context) => MyPlantsPage(), // <-- add this
-        '/newplant': (context) => NewPlantPage(), // optional
-      },
     );
   }
 }
